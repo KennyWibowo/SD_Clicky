723918dde0900fd626851217523f7b0df04bd6bf
Joseph Thomas <joseph.thomas.c@gmail.com>
2015-10-03 13:18:34 -0700

    changes

--- utils/utils.js
+++ utils/utils.js
@@ -17,4 +17,54 @@ module.exports = {
       return classPass;
 
    }
+   classCreate:function( className, classSize, lectureTimes, user, callback)
+   {
+      if(user.type != "teacher")
+      {
+         var error = new Error("Sorry, only teachers can create classes.");
+         return callback(error);
+      }
+      if( !user.classes )
+      {
+         user.classes = {};
+      }
+      if( !user.classes[className] )
+      {
+         var error = new Error("Sorry, a class with this name has already been created.");
+         return callback(error);
+      }
+      if( classSize < 1)
+      {
+         var error = new Error("You need at least one student in your class.");
+         return callback(error);
+      }
+      if( lectureNumber < 1)
+      {
+         var error = new Error("You need at least one lecture in your class.");
+         return callback(error); 
+      }
+      user.classes[className]= {
+         "classSize": classSize,
+         "lectures":{},
+         "lectureTimes": lectureTimes};
+
+  
+   }
+   lectureCreate:function(user, className, lectureName, lectureTime)
+   {
+      if(!user.classes)
+      {
+         var error = new Error("You need to create a class first.");
+         return callback(error); 
+      }
+      if( !user.classes[className])
+      {
+         var error = new Error("Pick one of the classes you've already created");
+         return callback(error); 
+      }
+      user.classes[className].lectures[lectureName] = {
+         "questions": {},
+         "lectureTime": lectureTime};
+      }
+   }
 }
