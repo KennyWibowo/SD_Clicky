32c1e84b64848a02a435ba168a8fe10cf263829a
Kenny Wibowo <kewibowo@ucsd.edu>
2015-10-03 20:41:49 -0700

    formatted utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,9 @@
-var  symbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", 
-                            "9", "a", "b", "c", "d", "e", "f", "g", 
-                            "h", "i", "j", "k", "l", "m", "n", "o",
-                            "p", "q", "r", "s", "t", "u", "v,", "w",
-                            "x", "y", "z"]
+var symbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8",
+    "9", "a", "b", "c", "d", "e", "f", "g",
+    "h", "i", "j", "k", "l", "m", "n", "o",
+    "p", "q", "r", "s", "t", "u", "v,", "w",
+    "x", "y", "z"
+]
 
 var classes = {
     1: {
@@ -11,16 +12,16 @@ var classes = {
         "pupils": ["Swashbuckler"],
         "lectures": {
             1: {
-                "questions" {
-                  1: {
-                     "question": "How do you get swole?",
-                     "a": "Be Siraj",
-                     "b": "Be Joe",
-                     "c": "Do nothing",
-                     "d": "Really do nothing",
-                     "e": "Eat sushi"
-                     "correct": "b"
-                  }
+                "questions": {
+                    1: {
+                        "question": "How do you get swole?",
+                        "a": "Be Siraj",
+                        "b": "Be Joe",
+                        "c": "Do nothing",
+                        "d": "Really do nothing",
+                        "e": "Eat sushi",
+                        "correct": "b"
+                    }
                 }
             }
         }
@@ -28,105 +29,88 @@ var classes = {
 }
 
 module.exports = {
-   randomGenerator: function( passLength)
-   {
+    randomGenerator: function(passLength) {
 
-      var classPass = "";
+        var classPass = "";
 
-      for( i = 0; i < passLength; i++)
-      {
-         classPass += symbols[Math.rand()*36];
-      }
+        for (i = 0; i < passLength; i++) {
+            classPass += symbols[Math.rand() * 36];
+        }
 
-      return classPass;
+        return classPass;
 
-   }
-   classCreate:function( className, classSize, lectureTimes, user, callback)
-   {
-      if(user.type != "teacher")
-      {
-         var error = new Error("Sorry, only teachers can create classes.");
-         return callback(error);
-      }
-      if( !user.classes )
-      {
-         user.classes = {};
-      }
-      if( !user.classes[className] )
-      {
-         var error = new Error("Sorry, a class with this name has already been created.");
-         return callback(error);
-      }
-      if( classSize < 1)
-      {
-         var error = new Error("You need at least one student in your class.");
-         return callback(error);
-      }
-      if( lectureNumber < 1)
-      {
-         var error = new Error("You need at least one lecture in your class.");
-         return callback(error); 
-      }
-      user.classes[className]= {
-         "classSize": classSize,
-         "lectures":{},
-         "lectureTimes": lectureTimes};
+    },
+    classCreate: function(className, classSize, lectureTimes, user, callback) {
+        if (user.type != "teacher") {
+            var error = new Error("Sorry, only teachers can create classes.");
+            return callback(error);
+        }
+        if (!user.classes) {
+            user.classes = {};
+        }
+        if (!user.classes[className]) {
+            var error = new Error("Sorry, a class with this name has already been created.");
+            return callback(error);
+        }
+        if (classSize < 1) {
+            var error = new Error("You need at least one student in your class.");
+            return callback(error);
+        }
+        if (lectureNumber < 1) {
+            var error = new Error("You need at least one lecture in your class.");
+            return callback(error);
+        }
+        user.classes[className] = {
+            "classSize": classSize,
+            "lectures": {},
+            "lectureTimes": lectureTimes
+        };
 
-  
-   }
-   lectureCreate:function(user, className, lectureName, lectureTime)
-   {
-      if(!user.classes)
-      {
-         var error = new Error("You need to create a class first.");
-         return callback(error); 
-      }
-      if( !user.classes[className])
-      {
-         var error = new Error("Pick one of the classes you've already created");
-         return callback(error); 
-      }
-      if( user.classes[className].lectures[lectureName] )
-      {
-         var error = new Error("You've already created a lecture with that name");
-         return callback(error); 
-      }
-      user.classes[className].lectures[lectureName] = {
-         "questions": {},
-         "lectureTime": lectureTime};
-      }
-   }
-   questionAdd:function(user, className, lectureName, questionName, question, a, b, c, d, e, answer)
-   {
-      if(!user.classes)
-      {
-         var error = new Error("You need to create a class first.");
-         return callback(error); 
-      }
-      if( !user.classes[className])
-      {
-         var error = new Error("Pick one of the classes you've already created");
-         return callback(error); 
-      }
-      if(!user.classes[className].lectures[lectureName])
-      {
-         var error = new Error("Pick one of the lectures you've already created");
-         return callback(error); 
-      }
-      if(user.classes[className].lectures[lectureName].questions[questionName])
-      {
-         var error = new Error("You've already created a question with this name");
-         return callback(error); 
-      }
-      user.classes[className].lectures[lectureName].questions[questionName] = {
-         "question": question,
-         "a": a,
-         "b": b,
-         "c": c,
-         "d": d,
-         "e": e,
-         "answer": answer};
-      }
-   }
 
+    },
+    lectureCreate: function(user, className, lectureName, lectureTime) {
+        if (!user.classes) {
+            var error = new Error("You need to create a class first.");
+            return callback(error);
+        }
+        if (!user.classes[className]) {
+            var error = new Error("Pick one of the classes you've already created");
+            return callback(error);
+        }
+        if (user.classes[className].lectures[lectureName]) {
+            var error = new Error("You've already created a lecture with that name");
+            return callback(error);
+        }
+        user.classes[className].lectures[lectureName] = {
+            "questions": {},
+            "lectureTime": lectureTime
+        }
+    },
+    questionAdd: function(user, className, lectureName, questionName, question, a, b, c, d, e, answer) {
+        if (!user.classes) {
+            var error = new Error("You need to create a class first.");
+            return callback(error);
+        }
+        if (!user.classes[className]) {
+            var error = new Error("Pick one of the classes you've already created");
+            return callback(error);
+        }
+        if (!user.classes[className].lectures[lectureName]) {
+            var error = new Error("Pick one of the lectures you've already created");
+            return callback(error);
+        }
+        if (user.classes[className].lectures[lectureName].questions[questionName]) {
+            var error = new Error("You've already created a question with this name");
+            return callback(error);
+        }
+        user.classes[className].lectures[lectureName].questions[questionName] = {
+            "question": question,
+            "a": a,
+            "b": b,
+            "c": c,
+            "d": d,
+            "e": e,
+            "answer": answer
+        };
+    }
 }
